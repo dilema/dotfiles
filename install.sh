a9f0e93d60a2cbbf4e7586f48a5d43c60461c67f
dilema <mart.leduc@hotmail.fr>
2019-12-30 03:16:23 -0800

    Install git config files

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-DOTFILES_DIR="$(dirname "$(readlink "$0")")"
+DOTFILES_DIR="$( cd -P "$( dirname "$0" )" && pwd )"
 
 if [ ! -d "$DOTFILES_DIR/backup" ]
 then
   mkdir "$DOTFILES_DIR/backup"
 fi
+
+
+# GIT CONFING INSTALLATION
+# ========================
+echo "Trying to install git config file..."
+
+if [ -e "$HOME/.gitconfig" ]
+then
+  mv "$HOME/.gitconfig" "$DOTFILES_DIR/backup/.gitconfig"
+fi
+
+ln -hfs "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 
